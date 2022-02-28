7b212a10ec44553a8d4643493945cd8e95f04b4c
Эмине Султанова <fely08@mail.ru>
2022-02-28 18:24:42 +0300

    Inishial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main<bonus> {
+    public static void main(String[] args) {
+
+       int amount = 100; // изначальная сумма на счету
+
+        int replenishment = 1100;// сумма пополнения счета
+
+        int oneBonus = 100; //столько нужно потратить, чтоб получить 1 бонусный рубль
+
+        int bonus = replenishment > 1000 ? replenishment / oneBonus : 0;
+
+        int finalAccount = amount + bonus; //итоговая сумма на счету(с учетом бонусов)
+
+        System.out.println("Ваш бонус составляет: " + bonus + ". Итоговая сумма на счету:" + finalAccount);
+    }
+    }
